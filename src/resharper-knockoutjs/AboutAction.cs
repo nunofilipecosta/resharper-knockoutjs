3cdcc60bfb3f9fad24d77c97d069788910b0ba93
Nuno Costa <nunofilipe.costa@gmail.com>
2014-07-26 16:28:53 +0200

    FORCED CHECK IN

---
+++ src/resharper-knockoutjs/AboutAction.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Forms;
+using JetBrains.ActionManagement;
+using JetBrains.Application.DataContext;
+
+namespace resharper_knockoutjs
+{
+  [ActionHandler("resharper-knockoutjs.About")]
+  public class AboutAction : IActionHandler
+  {
+    public bool Update(IDataContext context, ActionPresentation presentation, DelegateUpdate nextUpdate)
+    {
+      // return true or false to enable/disable this action
+      return true;
+    }
+
+    public void Execute(IDataContext context, DelegateExecute nextExecute)
+    {
+      MessageBox.Show(
+        "resharper-knockoutjs\nNuno Costa\n\nresharper-knockoutjs",
+        "About resharper-knockoutjs",
+        MessageBoxButtons.OK,
+        MessageBoxIcon.Information);
+    }
+  }
+}
---
+++ src/resharper-knockoutjs/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using JetBrains.ActionManagement;
+using JetBrains.Application.PluginSupport;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("resharper-knockoutjs")]
+[assembly: AssemblyDescription("resharper-knockoutjs")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Nuno Costa")]
+[assembly: AssemblyProduct("resharper-knockoutjs")]
+[assembly: AssemblyCopyright("Copyright © Nuno Costa, 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: ActionsXml("resharper-knockoutjs.Actions.xml")]
+
+// The following information is displayed by ReSharper in the Plugins dialog
+[assembly: PluginTitle("resharper-knockoutjs")]
+[assembly: PluginDescription("resharper-knockoutjs")]
+[assembly: PluginVendor("Nuno Costa")]
